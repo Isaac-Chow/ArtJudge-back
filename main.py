9bf7da9bf3285dc45ecf849e42b86f9b4dd45e7e
Isaac-Chow <99174054+Isaac-Chow@users.noreply.github.com>
2026-09-17 12:34:37 +0000

    addedd render path

--- main.py
+++ main.py
@@ -60,6 +60,7 @@ app.add_middleware(
         "http://localhost:3000",   # Docker frontend
         "http://127.0.0.1:5173",
         "http://127.0.0.1:3000",
+        "https://artjudge-front.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
